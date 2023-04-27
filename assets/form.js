9b729ee9461e495584993feb197e05dd37269869
Sselenso <122260649+Sselenso@users.noreply.github.com>
2023-04-27 16:36:48 +0300

    27.04_16:36

--- assets/form.js
+++ assets/form.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var showFormBtn = document.getElementById("show-form");
   var closeFormBtn = document.getElementById("close-form");
   var formPopup = document.getElementById("myForm");
 
   // Show the form when "Sign up" button is clicked
-  showFormBtn.addEventListener("click", function() {
+  showFormBtn.addEventListener("click", function () {
     formPopup.style.display = "block";
   });
 
   // Hide the form when "Cancel" button is clicked
-  closeFormBtn.addEventListener("click", function() {
+  closeFormBtn.addEventListener("click", function () {
     formPopup.style.display = "none";
   });
 });
@@ -19,21 +19,23 @@ function sendTelegramMessage(form) {
   const captain = form.querySelector('input[name="captain"]').value;
   const team = form.querySelector('textarea[name="team"]').value;
 
-  const message = `New application:%0ACaptain: ${captain}%0ATeam: ${team}`;
+  const message = `New application:\nCaptain: ${captain}\nTeam: ${team}`;
 
-  const telegramBotToken = '6233062235:AAHqGmnsqpQedV6OzKSd23xJYIVjRYey_fc';
-  const telegramChatId = '742627429';
+  const telegramBotToken = "6233062235:AAHqGmnsqpQedV6OzKSd23xJYIVjRYey_fc";
+  const telegramChatId = "742627429";
 
-  const telegramUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${telegramChatId}&text=${encodeURIComponent(message)}`;
+  const telegramUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${telegramChatId}&text=${encodeURIComponent(
+    message
+  )}`;
   fetch(telegramUrl)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       if (data.ok) {
         // Если сообщение успешно отправлено, открываем чат
         window.open(url);
       } else {
         // Если произошла ошибка, выводим сообщение об ошибке
-        alert('Ошибка при отправке сообщения в Telegram');
+        alert("Ошибка при отправке сообщения в Telegram");
       }
     });
 }
